ce2907eb3fcd6e66008373d0bf1b0e193a06d4f0
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-07-22 05:55:53 +0000

    Add more comments

--- printf-test/_printf.c
+++ printf-test/_printf.c
@@ -20,22 +20,25 @@ int _printf(const char * const format, ...)
 	{
 		if (format[i] != '%') /* normal character */
 		{
-			_putchar(format[i]);
-			i++;
-			count++;
+			_putchar(format[i]); /* print character */ 
+			count++; /* increase character count */
+			i++; /* go to the next char in format */
 		}
 		else /* i.e. there is a conversion specifier */
 		{
-			i++; 
+			i++; /* get the character after the % symbol */
+			/* get print function for the conversion specifier */
 			f = get_print_func(format[i]);
-			if (f == NULL) 
+			if (f == NULL) /* invalid conversion specifier */ 
 			{
+				/* print % symbol and character */
 				_putchar('%');
 				_putchar(format[i]);
 				count = count + 2;
 			}
 			else
 			{
+				/* call print function and add to count */
 				count += f(args);
 			}
 			i++;
